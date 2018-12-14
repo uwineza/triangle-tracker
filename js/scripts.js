7f21e3588f2da47a70b3718d69a66aa25b9327ed
UWINEZA FLORENTINE <niyinezafofo@gmail.com>
2018-12-14 11:13:18 +0200

    changing alert to console function

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,19 @@ var side1=parseInt  (prompt("enter the value of side1"));
 var side2=parseInt(prompt("enter the value of side2"));
 var side3=parseInt(prompt("enter the value of side3"));
 var triangle=[side1,side2,side3];
-alert(triangle)
+console.log(triangle)
 var result1=side1+side2;
 var result2=side1+side3;
 var result3=side2+side3;
 if(side1===side2&&side1===side3){
-  alert("Triangle is Equilateral");
+  console.log("Triangle is Equilateral");
 }
 else if (side1===side2||side1===side3||side2===side3){
-  alert("Triangle is isoscele");
+  console.log("Triangle is isoscele");
 }
 else if(side1!=side2 && side1!=side3 && side2!=side3 && result1>side3 && result2>side2 && result3>side1){
- alert("Triangle is Scalene");
+ console.log("Triangle is Scalene");
 }
 else{
-  alert("Triagle can not be formed");
+  console.log("Triagle can not be formed");
 }
