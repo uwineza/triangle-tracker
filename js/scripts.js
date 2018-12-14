5603e98896b419f8d246eca065e0c646c8ad353f
UWINEZA FLORENTINE <niyinezafofo@gmail.com>
2018-12-14 15:08:34 +0200

    add imput bottoms

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,26 @@
-var side1 = parseInt(prompt("enter the value of side1"));
-var side2 = parseInt(prompt("enter the value of side2"));
-var side3 = parseInt(prompt("enter the value of side3"));
-var triangle = [side1, side2, side3];
-console.log(triangle);
-var result1 = side1 + side2;
-var result2 = side1 + side3;
-var result3 = side2 + side3;
-if (side1 === side2 && side1 === side3) {
-  console.log("Triangle is Equilateral");
-} else if (side1 === side2 || side1 === side3 || side2 === side3) {
-  console.log("Triangle is isoscele");
-} else if (
-  side1 != side2 &&
-  side1 != side3 &&
-  side2 != side3 &&
-  result1 > side3 &&
-  result2 > side2 &&
-  result3 > side1
-) {
-  console.log("Triangle is Scalene");
-} else {
-  console.log("Triagle can not be formed");
+function myTriangle() {
+  var side1 = parseInt(document.getElementById("side1").value);
+  var side2 = parseInt(document.getElementById("side2").value);
+  var side3 = parseInt(document.getElementById("side2").value);
+  var triangle = [side1, side2, side3];
+  console.log(triangle);
+  var result1 = side1 + side2;
+  var result2 = side1 + side3;
+  var result3 = side2 + side3;
+  if (side1 === side2 && side1 === side3) {
+    console.log("Triangle is Equilateral");
+  } else if (side1 === side2 || side1 === side3 || side2 === side3) {
+    console.log("Triangle is isoscele");
+  } else if (
+    side1 != side2 &&
+    side1 != side3 &&
+    side2 != side3 &&
+    result1 > side3 &&
+    result2 > side2 &&
+    result3 > side1
+  ) {
+    console.log("Triangle is Scalene");
+  } else {
+    console.log("Triagle can not be formed");
+  }
 }
