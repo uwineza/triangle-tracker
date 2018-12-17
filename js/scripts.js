1623764db3448e8b93a8d7ca9276809ebfeb46ca
UWINEZA FLORENTINE <niyinezafofo@gmail.com>
2018-12-17 09:12:39 +0200

    git commit -m-+--+

--- js/scripts.js
+++ js/scripts.js
@@ -8,23 +8,34 @@ function myTriangle() {
   var result1 = side1 + side2;
   var result2 = side1 + side3;
   var result3 = side2 + side3;
-  if(result1 > side3 &&
+
+  if (
+    side1 === side2 &&
+    side1 === side3 &&
+    result1 > side3 &&
     result2 > side2 &&
-    result3 > side1){
-   if (side1 === side2 && side1 === side3) {
+    result3 > side1
+  ) {
     console.log("Triangle is Equilateral");
     alert("Triangle is Equilateral");
-   } else if (side1 === side2 || side1 === side3 || side2 === side3) {
+  } else if (
+    (side1 === side2 || side1 === side3 || side2 === side3) &&
+    (result1 > side3 && result2 > side2 && result3 > side1)
+  ) {
     console.log("Triangle is isoscele");
-    alert("Triangle is isoscele")
-  } else 
+    alert("Triangle is isoscele");
+  } else if (
+    side1 != side2 &&
+    side1 != side3 &&
+    side2 != side3 &&
+    result1 > side3 &&
+    result2 > side2 &&
+    result3 > side1
   ) {
     console.log("Triangle is Scalene");
     alert("Triangle is Scalene");
-    }
-  };
-  else{
-  console.log("Triagle can not be formed");
-  alert("Triagle can not be formed");
-};
-};
+  } else {
+    console.log("Triagle can not be formed");
+    alert("Triagle can not be formed");
+  }
+}
